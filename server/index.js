a6171e93c9ed1391ada435b689cb83bf76490ec7
Melissa Schaefer <mschaefer@xpanxion.com>
2023-06-07 17:18:35 -0400

    update

--- server/index.js
+++ server/index.js
@@ -38,21 +38,29 @@ app.get('/message', cors(corsOptions), async (req, res) => {
         //     res.send(result[0])
         // })
         
-        //CarBody
-        // const [result] = await promisePool.execute("insert into car (make, model, color, price) values (?, ?, ?, ?)", ["Toyota", "Corolla", "Gray", 30000])
-        // const newCarId = result.insertId;
-        // const [newCar] = await promisePool.query("select * from car where car_id = ?", newCarId)
-        // res.send(newCar[0]);
+        //post card
+        app.post("/car/", cors(corsOptions), async (req, res) => {
+        const [result] = await promisePool.execute("insert into car (make, model, color, price) values (?, ?, ?, ?)", ["Toyota", "Corolla", "Gray", 30000])
+        const newCarId = result.insertId;
+        const [newCar] = await promisePool.query("select * from car where car_id = ?", newCarId)
+        res.send(newCar[0]);
       
         //put car
+        //     app.put('/car/', cors(corsOptions), async (req, res) => { 
+        //     const { make, model, color, price, carId }= req.body;
+        //     const UPDATE_CAR = "update car set make = ?, model = ?, color = ?, price = ? where car_id = ?";
+        //     const result = await promisePool.query(UPDATE_CAR, [make, model, color, price, carId] )
+        //     console.log(result[0])
+        //     res.send(result[0])
+        // })
 
-        app.put('/car/', cors(corsOptions), async (req, res) => { 
-            const { make, model, color, price, carId }= req.body;
-            const UPDATE_CAR = "update car set make = ?, model = ?, color = ?, price = ? where car_id = ?";
-            const result = await promisePool.query(UPDATE_CAR, [make, model, color, price, carId] )
-            console.log(result[0])
-            res.send(result[0])
-        })
+        // //delete car
+        // app.delete('/car/:carID', cors(corsOptions), async (req, res) => {
+        // const { carID }= req.params 
+        // let result = await promisePool.query('SELECT * FROM car where car_id=?', [carID])
+        // console.log(result[0])
+        // res.send(result[0])
+        //  })
 
    
         
