2e9f12be797b867f86fbb70f87ce3be815b728d5
Melissa Schaefer <mschaefer@xpanxion.com>
2023-06-08 16:31:42 -0400

    update

--- server/index.js
+++ server/index.js
@@ -21,15 +21,22 @@ app.use(express.urlencoded({extended: true}))
     })
 
         //carID
-        // app.get('/car/:carID', cors(corsOptions), async (req, res) => {
-        // const { carID }= req.params 
-        // let result = await promisePool.query('SELECT * FROM car where car_id=?', [carID])
-        // console.log(result[0])
-        // res.send(result[0])
+        app.get('/car/:carID', cors(corsOptions), param('id').isNumeric(),
+        async (req, res) => {
+             
+            const errors = validationResult(req)
         
-        //  })
-    
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+        }
 
+        let car_id = req.params['id'];
+        const result = await promisePool.query('SELECT * FROM car where car_id=?', [car_id])
+        
+        result[0]? res.send(result[0]):res.status(404).send({message: 'not found.'});
+        });
+        
+        
         //CarMake
         // app.get('/car/:make', cors(corsOptions), async (req, res) => { 
         //     const make  = req.params.make;
@@ -38,15 +45,15 @@ app.use(express.urlencoded({extended: true}))
         //     res.send(result[0])
         // })
         
-        //post card
-        app.post("/car/", cors(corsOptions), async (req, res) => {
-        const [result] = await promisePool.execute("insert into car (make, model, color, price) values (?, ?, ?, ?)", ["Toyota", "Corolla", "Gray", 30000])
-        const newCarId = result.insertId;
-        const [newCar] = await promisePool.query("select * from car where car_id = ?", newCarId)
-        console.log(newCar[0])
-        res.send(newCar[0]);
+        //post car
+        // app.post("/car/", cors(corsOptions), async (req, res) => {
+        // const [result] = await promisePool.execute("insert into car (make, model, color, price) values (?, ?, ?, ?)", ["Toyota", "Corolla", "Gray", 30000])
+        // const newCarId = result.insertId;
+        // const [newCar] = await promisePool.query("select * from car where car_id = ?", newCarId)
+        // console.log(newCar[0])
+        // res.send(newCar[0]);
 
-        })
+        // })
       
         //put car
         //     app.put('/car/', cors(corsOptions), async (req, res) => { 
@@ -58,14 +65,22 @@ app.use(express.urlencoded({extended: true}))
         // })
 
         // //delete car
-        app.delete('/car/:carID', cors(corsOptions), async (req, res) => {
-        const { carID }= req.params 
-        let result = await promisePool.query('SELECT * FROM car where car_id=?', [carID])
-        // const DELETE_CAR = "delete from car where car_id = ?"
-        console.log(result[0])
-        res.send(result[0])
-         })
+        // app.delete('/car/:carID', cors(corsOptions), async (req, res) => {
+        // const { carID }= req.params 
+        // let result = await promisePool.query('SELECT * FROM car where car_id=?', [carID])
+        // // const DELETE_CAR = "delete from car where car_id = ?"
+        // console.log(result[0])
+        // res.send(result[0])
+        //  })
 
+
+        //api hardening
+
+        // app.get('/person/:id', cors(corsOptions), async (req, res) => {
+        // const personId = req.params['id']
+        // const person = await mySqlProxy.selectPersonById(personId)
+        // person ? res.send(person) : res.status(404).send({message: 'Not found.'})
+        // })
    
         
         
